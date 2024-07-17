6d3f00548f450b939c1a0cd36fe1830fe1797352
Arlette Miranda <arlette.miranda.ch@gmail.com>
2024-07-16 20:24:37 -0600

    Se realizan cambios en funcionalidad

--- filtro.js
+++ filtro.js
@@ -1,64 +1,92 @@
 // Tenemos un li de productos
+/* Se identa el documento */
+/* Se crea carpeta para imagenes */
 
+
+/* Se modifica ruta de imagenes */
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+  { nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "public/img/taco-negro.jpg" },
+  { nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "public/img/taco-azul.jpg" },
+  { nombre: "Bota negra", tipo: "bota", color: "negro", img: "public/img/bota-negra.jpg" },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "public/img/bota-azul.jpg" },
+  { nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "public/img/zapato-rojo.jpg" }
+]; /* Se agrega ; */
 
-const li = document.getElementsById("lista-de-productos") /* Se corrige a solicitar por id en vez de name */
-/* const $i =  */document.querySelector('.input'); /* Se quita la variable ya que se dice la referencia */
+/* Se renombra variable li, en todo el documento, para poner una variable que mencione a que se hace referencia */
+const listaProducto = document.getElementById("lista-de-productos") /* Se corrige a solicitar por id en vez de name */
+const inputDoc = document.querySelector('input'); /* Se renombra variable ya que no debe inciar con $ y se omite . en imput*/
 
-for (let i = 0; i < productos.length; i++) {
-  let d = document.createElement("div") /* Se cambia tipo de variable de var a let */
-  d.classList.add("producto")
+/* Se define función que se llama al finalizar esta */
+/* Se define el parameto que recibirá la función */
+function displayProductos(objeto) {
 
-  let ti = document.createElement("p") /* Se cambia tipo de variable de var a let */
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  let imagen = document.createElement("img"); /* Se cambia tipo de variable de var a let */
-  imagen.setAttribute('src', productos[i].img);
+  /* Se acomoda while dentro de función ya que hace referencia a child que se encuentran en esta funcion */
+  while (listaProducto.firstChild) {
+    listaProducto.removeChild(listaProducto.firstChild);
+  }
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+  /* Se agregan ; dentro de for */
 
-  li.appendChild(d)
-  shopContent.append(content);
+  for (let i = 0; i < objeto.length; i++) {
+    /* Se renombra variable di a divProducto, para hacer mejor referncia */
+    let divProducto = document.createElement("div"); /* Se cambia tipo de variable de var a let */
+    divProducto.classList.add("producto");
+
+    /* Se renombra variable ti a titulo, para hacer mejor referncia */
+    let titulo = document.createElement("p"); /* Se cambia tipo de variable de var a let */
+    titulo.classList.add("titulo");
+    titulo.textContent = objeto[i].nombre;/* se pone el parametro de la función, objeto, en vez de producto */
+
+    let imagen = document.createElement("img"); /* Se cambia tipo de variable de var a let */
+    imagen.setAttribute('src', objeto[i].img); /* se pone el parametro de la función, objeto, en vez de producto */
+
+    divProducto.appendChild(titulo); /* Se agrega ; */
+    divProducto.appendChild(imagen); /* Se agrega ; */
+
+    listaProducto.appendChild(divProducto); /* Se agrega ; */
+  }
+}
+/* Se llama la función antes creada */
+displayProductos(productos); /* se agrega ; */
+
+/* Se acomoda la función filtrado antes de se llamada  en el botón*/
+const filtrado = (productos, texto) => { /* Se quita =[] para definir el parametro productos */
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
 }
 
-displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
-botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
-  }
+botonDeFiltro.onclick = function () {
 
-  const texto = $i.value;
-  console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    let d = document.createElement("div") /* Se cambia tipo de variable de var a let */
-    d.classList.add("producto")
+  const texto = inputDoc.value.toLocaleLowerCase();/* Se agrega lowercase para hacer mínusculas el input del usuario */ 
+  console.log(texto); ///REVISAR ////
   
-    let ti = document.createElement("p") /* Se cambia tipo de variable de var a let */
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+  const productosFiltrados = filtrado(productos, texto);
+
+  displayProductos(productosFiltrados);
+  
+  /* Esta segundo for lo cometo para no eliminarlo,
+  Pero se eliminaría ya que hace que al realizar el filtro el reusltado se duplique*/
+
+  
+ /*  for (let i = 0; i < productosFiltrados.length; i++) {
+    // Se renombra variable para mayor referencia //
+    let divFiltrados = document.createElement("div") // Se cambia tipo de variable de var a let 
+    divFiltrados.classList.add("producto")
     
-    let imagen = document.createElement("img"); /* Se cambia tipo de variable de var a let */
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
-  }
-}
+    // Se renombra variable para mayor referencia 
+    let titutloFiltrados = document.createElement("p") // Se cambia tipo de variable de var a let
+    titutloFiltrados.classList.add("titulo")
+    titutloFiltrados.textContent = productosFiltrados[i].nombre
 
-const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+    let imagen = document.createElement("img"); // Se cambia tipo de variable de var a let 
+    imagen.setAttribute('src', productosFiltrados[i].img);
+
+    divFiltrados.appendChild(titutloFiltrados)
+    divFiltrados.appendChild(imagen)
+
+    listaProducto.appendChild(divFiltrados)
+  } */
+
+};
+
